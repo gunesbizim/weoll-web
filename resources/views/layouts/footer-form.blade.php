4b36cf9cadcd94020582b92c486950631d7499f0
gunesbizim <gnsbzm@gmail.com>
2022-07-04 11:54:20 +0300

    packages, footer form, 404

---
+++ resources/views/layouts/footer-form.blade.php
@@ -0,0 +1,56 @@
+<div class="spacer-80"></div>
+<div class="container footer-contact">
+    <div class="row">
+        <h2>BİZİMLE İLETİŞİME GEÇİN</h2>
+        <form action="" class="d-flex flex-column form-footer border-r-5">
+            <div class="row d-flex">
+                <div class="form-group col-md-6 col-xs-12 d-flex flex-column">
+                    <label for="name">Ad</label>
+                    <input type="text" name="name">
+                </div>
+                <div class="form-group col-md-6 col-xs-12 d-flex flex-column">
+                    <label for="surname">Soyad</label>
+                    <input type="text" name="surname">
+                </div>
+            </div>
+            <div class="row d-flex">
+                <div class="form-group col-md-6 col-xs-12 d-flex flex-column">
+                    <label for="firmname">Firma Adı</label>
+                    <input type="text" name="firmname">
+                </div>
+                <div class="form-group col-md-6 col-xs-12 d-flex flex-column">
+                    <label for="position">Ünvan</label>
+                    <input type="text" name="position">
+                </div>
+            </div>
+
+            <div class="row d-flex">
+                <div class="form-group col-md-6 col-xs-12 d-flex flex-column">
+                    <label for="email">E-Posta</label>
+                    <input type="email" name="email">
+                </div>
+                <div class="form-group col-md-6 col-xs-12 d-flex flex-column">
+                    <label for="phone">Telefon</label>
+                    <input type="phone" name="phone">
+                </div>
+            </div>
+            <div class="row d-flex ">
+                <div class="form-group col-md-12 col-xs-12 d-flex flex-column flex-h-end">
+                    <label for="message"></label>
+                    <textarea name="message" id="" cols="30" rows="10"></textarea>
+                    <button type="submit" value="Gönder" class="form-submit">Gönder</button>
+                </div>
+            </div>
+            <div class="row d-flex flex-v-center">
+                <div class="contact-info phone col-md-2 text-right">
+                    +90 (212) 211 50 01
+                </div>
+                <div class="contact-info email col-md-2 text-right">
+                    info@weoll.com.tr
+                </div>
+            </div>
+
+        </form>
+    </div>
+</div>
+<div class="spacer-80"></div>
