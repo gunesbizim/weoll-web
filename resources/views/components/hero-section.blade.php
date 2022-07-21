a20df8b4b1b819aacb53d126848d88c7ce3208ef
gunesbizim <gnsbzm@gmail.com>
2022-07-21 13:52:58 +0300

    safari_only fix

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,7 +1,5 @@
-<div class="container fullpage fullwidth m-40 
-@if (isset($heroData->hero->safariOnly) && $heroData->hero->safariOnly) {{ 'safari_only' }} @endif
-">
-    {{-- {{ $heroData->hero->safariOnly ? 'safari_only' : '' }} --}}
+<div
+    class="container fullpage fullwidth m-40 {{ isset($heroData->hero->safariOnly) && $heroData->hero->safariOnly ? 'safari_only' : '' }}">
     <div class="row">
         <div class="d-flex col-md-6 col-xs-12 flex-column flex-v-center">
             {{-- @dd($heroData) --}}
